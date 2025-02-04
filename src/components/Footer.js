dcaf95d763bee08c90de26fef745ba133f0e58a6
mansavigarg <mansavigarg9919@gmail.com>
2025-02-04 22:48:57 +0530

    add files

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/mansavigarg/Shoping-Cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Github Link
+                    </a>
+
+                    <p>
+                        A Shopping Cart
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
